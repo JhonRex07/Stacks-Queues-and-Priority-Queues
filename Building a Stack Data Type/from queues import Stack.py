7c93ad53b4b5c9d688c69928040e80c0de95d1c4
JhonRex07 <jhonrexjusayan07@gmail.com>
2023-02-26 12:32:54 +0800

    Building Stack Data Type

---
+++ Building a Stack Data Type/from queues import Stack.py
@@ -0,0 +1,14 @@
+from queues import Stack
+
+lifo = Stack("1st", "2nd", "3rd")
+for element in lifo:
+    print(element)
+
+lifo = []
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("\n3rd")
+
+print(lifo.pop())
+print(lifo.pop())
+print(lifo.pop())
